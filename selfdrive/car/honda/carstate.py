62f7447f59be9f3a46c78de72a88dc735dba92b6
ttzhang0824 <ttzhang824@gmail.com>
2024-10-21 17:49:57 -0500

    2020 MDX

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -24,7 +24,7 @@ def get_can_messages(CP, gearbox_msg):
     ("POWERTRAIN_DATA", 100),
     ("CAR_SPEED", 10),
     ("VSA_STATUS", 50),
-    ("STEER_STATUS", 100),
+    # ("STEER_STATUS", 100),
     ("STEER_MOTOR_TORQUE", 0),  # TODO: not on every car
   ]
 
