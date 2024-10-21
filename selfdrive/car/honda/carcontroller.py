08bc1dadbf79f47fd28fae7421eb0cd812329ca0
ttzhang0824 <ttzhang824@gmail.com>
2024-10-21 16:52:21 -0500

    2020 MDX

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -203,37 +203,7 @@ class CarController(CarControllerBase):
     apply_steer = int(interp(-limited_steer * self.params.STEER_MAX,
                              self.params.STEER_LOOKUP_BP, self.params.STEER_LOOKUP_V))
 
-    if (CS.CP.carFingerprint in SERIAL_STEERING):
-      apply_steer = apply_driver_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS):  # //apply_driver_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS
-      self.apply_steer_last = apply_steer
-      if apply_steer > 229 and False:
-        apply_steer_orig = apply_steer
-        apply_steer = (apply_steer - 229) * 2 + apply_steer
-        if apply_steer > 240:
-            self.apply_steer_over_max_counter += 1
-            if self.apply_steer_over_max_counter > 3:
-                apply_steer = apply_steer_orig
-                self.apply_steer_over_max_counter = 0
-            else:
-                self.apply_steer_over_max_counter = 0
-      elif apply_steer < -229 and False:
-        apply_steer_orig = apply_steer
-        apply_steer = (apply_steer + 229) * 2 + apply_steer
-        if apply_steer < -240:
-            self.apply_steer_over_max_counter+= 1
-            if self.apply_steer_over_max_counter > 3:
-                apply_steer = apply_steer_orig
-                self.apply_steer_over_max_counter = 0
-            else:
-                self.apply_steer_over_max_counter = 0
-      else:
-        self.apply_steer_over_max_counter = 0
-
-    self.apply_steer_last = apply_steer
-
-    # steer torque is converted back to CAN reference (positive when steering right)
-    # apply_steer = -apply_steer
-    
+   
     if not self.CP.pcmCruiseSpeed:
       if not self.last_speed_limit_sign_tap_prev and CS.params_list.last_speed_limit_sign_tap:
         self.sl_force_active_timer = self.frame
