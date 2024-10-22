c8d4a09c8de6088d9e0705c6aa36d6e78df67595
ttzhang0824 <ttzhang824@gmail.com>
2024-10-22 14:25:46 -0500

    2020 MDX

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -126,7 +126,8 @@ class CarInterface(CarInterfaceBase):
         tire_stiffness_factor = 0.444
         ret.steerActuatorDelay = 0.1
         #ret.lateralTuning.pid.kf = 0.000035
-        #ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.115], [0.052]]
+
     
     elif candidate == CAR.ACURA_ILX:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 3840], [0, 3840]]  # TODO: determine if there is a dead zone at the top end
