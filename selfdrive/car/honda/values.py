5734da963bfdbea1a20c569e9c8973d377120e72
ttzhang0824 <ttzhang824@gmail.com>
2024-11-21 16:38:23 -0600

    2020 MDX

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -18,16 +18,16 @@ class CarControllerParams:
   # to apply some more braking if we're on a downhill slope.
   # Our controller should still keep the 2 second average above
   # -3.5 m/s^2 as per planner limits
-  NIDEC_ACCEL_MIN = -4.0  # m/s^2
+  NIDEC_ACCEL_MIN = -3.0  # m/s^2
   NIDEC_ACCEL_MAX = 1.6  # m/s^2, lower than 2.0 m/s^2 for tuning reasons
 
   NIDEC_ACCEL_LOOKUP_BP = [-1., 0., .6]
-  NIDEC_ACCEL_LOOKUP_V = [-4.8, 0., 2.0]
+  NIDEC_ACCEL_LOOKUP_V = [-3, 0., 1.6]
 
-  NIDEC_MAX_ACCEL_V = [0.5, 2.4, 1.4, 0.6]
-  NIDEC_MAX_ACCEL_BP = [0.0, 4.0, 10., 20.]
+  NIDEC_MAX_ACCEL_V = [0.4, 1.6, 1.4, 0.6]
+  NIDEC_MAX_ACCEL_BP = [0.0, 6.0, 10., 20.]
 
-  NIDEC_GAS_MAX = 198  # 0xc6
+  NIDEC_GAS_MAX = 180  # 0xc6
   NIDEC_BRAKE_MAX = 1024 // 4
 
   BOSCH_ACCEL_MIN = -3.5  # m/s^2
