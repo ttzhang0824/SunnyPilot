830f28f9c211c594450097706a9abeb57370ff97
ttzhang0824 <ttzhang824@gmail.com>
2024-11-28 11:45:12 -0600

    2020 MDX

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -201,13 +201,37 @@ class CarController(CarControllerBase):
     # **** process the car messages ****
 
     # steer torque is converted back to CAN reference (positive when steering right)
-    apply_steer = int(interp(actuators.steer * self.params.STEER_MAX,
+    apply_steer = int(interp(-limited_steer * self.params.STEER_MAX,
                              self.params.STEER_LOOKUP_BP, self.params.STEER_LOOKUP_V))
 
     if (self.CP.carFingerprint in SERIAL_STEERING):
       apply_steer = apply_driver_steer_torque_limits(apply_steer, self.apply_steer_last, CS.out.steeringTorque, LKAS_LIMITS, ss=True)
       self.apply_steer_last = apply_steer
-      
+
+      if apply_steer > 229 and False:
+        apply_steer_orig = apply_steer
+        apply_steer = (apply_steer - 229) * 2 + apply_steer
+        if apply_steer > 240:
+            self.apply_steer_over_max_counter += 1
+            if self.apply_steer_over_max_counter > 3:
+                apply_steer = apply_steer_orig
+                self.apply_steer_over_max_counter = 0
+            else:
+                self.apply_steer_over_max_counter = 0
+      elif apply_steer < -229 and False:
+        apply_steer_orig = apply_steer
+        apply_steer = (apply_steer + 229) * 2 + apply_steer
+        if apply_steer < -240:
+            self.apply_steer_over_max_counter+= 1
+            if self.apply_steer_over_max_counter > 3:
+                apply_steer = apply_steer_orig
+                self.apply_steer_over_max_counter = 0
+            else:
+                self.apply_steer_over_max_counter = 0
+      else:
+        self.apply_steer_over_max_counter = 0
+    
+    
     apply_steer = -apply_steer
 
     #if apply_steer == 0 or CS.out.vEgo < (10 * CV.MPH_TO_MS):
